316262a47c74cc257ea46e01ef62c6c5edb3c35f
JuiletteD <72203353+JuiletteD@users.noreply.github.com>
2021-01-27 16:22:31 +0100

    Delete Score directory

--- Score/Tableau.js
+++
@@ -1,1 +0,0 @@
-Tableau=[{"score":"51660","pseudo":"glen","date":"2021-01-26","temps":"00:39"},{"score":"52260","pseudo":"glen","date":"2021-01-26","temps":"00:29"},{"score":"52440","pseudo":"glen","date":"2021-01-26","temps":"00:26"}];
